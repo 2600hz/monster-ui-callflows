2c308068bbbabbe89b5ec9e80f2881a6de0624f0
Jean-Roch Maitre <jr@2600hz.com>
2015-01-29 14:23:14 -0800

    UI-1100: Minor changes on the Pull-Request

--- app.js
+++ app.js
@@ -245,16 +245,16 @@ define(function(require){
 			var formattedList = [];
 
 			_.each(data.data, function(callflow) {
-				var listNumbers = callflow.numbers || '-',
-				isFeatureCode = callflow.featurecode !== false && !_.isEmpty(callflow.featurecode);
+				var listNumbers = (callflow.numbers || '-').toString(),
+					isFeatureCode = callflow.featurecode !== false && !_.isEmpty(callflow.featurecode);
 
 				if(!isFeatureCode) {
 					if(callflow.name) {
-						callflow.description = listNumbers.toString();
+						callflow.description = listNumbers;
 						callflow.title = callflow.name;
 					}
 					else {
-						callflow.title = listNumbers.toString()
+						callflow.title = listNumbers;
 					}
 
 					formattedList.push(callflow);
