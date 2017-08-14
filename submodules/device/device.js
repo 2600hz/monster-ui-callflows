6e8835c7269a80eb69a03e64a5f004e68a048879
Jean-Roch Maitre <jr@2600hz.com>
2017-08-14 13:58:50 -0700

    UI-2868: only set invite_format to contact for devices with SIP creds
    Conflicts:
    	submodules/device/device.js

--- submodules/device/device.js
+++ submodules/device/device.js
@@ -880,7 +880,12 @@ define(function(require){
 				delete data.ringtones.external;
 			}
 
-			if($.inArray(data.device_type, ['fax', 'mobile', 'softphone', 'sip_device', 'smartphone']) < 0) {
+			// For devices who don't have sip creds, we need to use username, for sip url we already set it to "route", and for the others, the default is applied: "contact"
+			if ($.inArray(data.device_type, ['landline', 'cellphone']) >= 0) {
+				data.sip.invite_format = 'username';
+			}
+
+			if ($.inArray(data.device_type, ['fax', 'mobile', 'softphone', 'sip_device', 'smartphone']) < 0) {
 				delete data.call_restriction;
 			}
 
